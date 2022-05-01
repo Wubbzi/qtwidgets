9e395dbbd2efdf44aeb9d1d22daadf0a65395d23
chris <chris.souza3425@gmail.com>
2022-05-01 03:25:31 -0500

    Add horz

--- widgets/horizontalDivider.py
+++ widgets/horizontalDivider.py
@@ -5,30 +5,39 @@ from PyQt6.QtGui import QStaticText
 class HorizontalDivider(QtWidgets.QWidget):
     def __init__(self, parent=None, name=''):
         super(HorizontalDivider, self).__init__(parent)
-        self.__labelHeight = 0
-        self.__labaelWidth = 0
+        self.__name = name
         self.__label = None
+        self.__spacing = None
+        self.__labelWidth = 0
+        self.__labelHeight = 0
+
         self.palette = QtWidgets.QApplication.instance().palette()
         self.setSpacing(6)
         self.setName(name)
 
-    def setName(self, name):
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    def setName(self, name: str) -> None:
+        """The name on the label to the left of the divider."""
         self.__name = name
         self.__label = QStaticText(name)
         self.__label.setText(name)
         self.__calculateSize()
 
-    def setSpacing(self, width):
-        """
-        Sets the gap width between the divider and text.
-        """
+    def label(self) -> QStaticText:
+        return self.__label
+
+    def spacing(self) -> int:
+        return self.__spacing
+
+    def setSpacing(self, width: int) -> None:
+        """Sets the gap width between the divider and text."""
         self.__spacing = width
         self.update()
 
-    def spacing(self):
-        return self.__spacing
-
-    def paintEvent(self, event):
+    def paintEvent(self, event) -> None:
         painter = QtGui.QPainter(self)
         pen = QtGui.QPen(self.palette.color(QtGui.QPalette.ColorGroup.Active, QtGui.QPalette.ColorRole.Text), 1)
         painter.setPen(pen)
@@ -39,11 +48,11 @@ class HorizontalDivider(QtWidgets.QWidget):
 
         pen = QtGui.QPen(self.palette.color(QtGui.QPalette.ColorRole.Mid), 1)
         painter.setPen(pen)
-        painter.drawLine(self.__labaelWidth, cy, self.width(), cy)
+        painter.drawLine(self.__labelWidth, cy, self.width(), cy)
 
-    def __calculateSize(self):
+    def __calculateSize(self) -> None:
         fm = self.fontMetrics()
         self.__labelHeight = fm.height()
-        self.__labaelWidth = fm.horizontalAdvance(self.__name) + self.__spacing
+        self.__labelWidth = fm.horizontalAdvance(self.__name) + self.__spacing
         self.setFixedHeight(self.__labelHeight - 1)
         self.update()
